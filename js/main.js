b42e27df01a42a525885483444292511dd74c126
rdmug1421 <ralmughidi@gmail.com>
2022-09-15 13:06:08 +0300

    review swiper section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
-    menu.classList.toggle('move')
-}
+        menu.classList.toggle('move')
+    }
+    //reviews swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+
+});
