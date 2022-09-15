21f57ea5a7b712d1e6e35a531971a8ce514eecd0
rdmug1421 <ralmughidi@gmail.com>
2022-09-15 15:56:29 +0300

    contact form

--- js/main.js
+++ js/main.js
@@ -22,3 +22,46 @@ const swiper = new Swiper('.swiper', {
     // And if we need scrollbar
 
 });
+//EMAIL JS
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == "") {
+            emptyerror();
+        } else {
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
+
+function sendmail(name, email, msg) {
+    emailjs.send("service_np3rfdp", "template_8owxd58", {
+        to_name: name,
+        from_name: email,
+        message: msg,
+    });
+}
+
+
+function emptyerror() {
+    swal({
+        title: "Oh No....",
+        text: "Fields cannot be empty!",
+        icon: "error",
+    });
+}
+
+function success() {
+    swal({
+        title: "Email sent successfully",
+
+        icon: "success",
+    });
+}
